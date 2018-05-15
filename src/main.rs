769d7318e0ac67b7cb4c6cd43cb097ebf2089ddb
Johan Larsson <johan.larsson1989@gmail.com>
2018-05-15 20:21:21 +0100

    now correctly reads getopts arguments

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,20 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let program = args[0].clone();
+    let _program = args[0].clone();
     let mut opts = Options::new();
     opts.optflag("r", "read", "read data");
-    println!("Hello world!");
+    opts.optflag("e", "export", "export data");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => { panic!(f.to_string()) }
+    };
+    if matches.opt_present("r") {
+        println!("read");
+        return;
+    }
+    if matches.opt_present("e") {
+        println!("export");
+        return;
+    }
 }
