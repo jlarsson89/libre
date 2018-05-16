b84ff286c04c9c847c159ef8f9b70604ddfddefb
Johan Larsson <johan.larsson1989@gmail.com>
2018-05-16 22:21:49 +0100

    removed mut. package builds(forgot libusb native)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 extern crate getopts;
-//extern crate libusb;
+extern crate libusb;
 use getopts::Options;
 use std::env;
 
@@ -7,7 +7,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let _program = args[0].clone();
     let mut opts = Options::new();
-    /*let mut context = libusb::Context::new().unwrap();
+    let context = libusb::Context::new().unwrap();
     for mut device in context.devices().unwrap().iter() {
         let device_desc = device.device_descriptor().unwrap();
         println!("Bus {:03} Device {:03} ID: {:04x}:{:04x}",
@@ -15,7 +15,7 @@ fn main() {
                  device.address(),
                  device_desc.vendor_id(),
                  device_desc.product_id());
-    }*/
+    }
     opts.optflag("r", "read", "read data");
     opts.optflag("e", "export", "export data");
     let matches = match opts.parse(&args[1..]) {
