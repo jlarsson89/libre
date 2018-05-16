6050f5119cf40b48c1c93e10c4d3a164d0c35d7d
Johan Larsson <johan.larsson1989@gmail.com>
2018-05-16 20:28:15 +0100

    added libusb but breaks compilation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate getopts;
+//extern crate libusb;
 use getopts::Options;
 use std::env;
 
@@ -6,6 +7,15 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let _program = args[0].clone();
     let mut opts = Options::new();
+    /*let mut context = libusb::Context::new().unwrap();
+    for mut device in context.devices().unwrap().iter() {
+        let device_desc = device.device_descriptor().unwrap();
+        println!("Bus {:03} Device {:03} ID: {:04x}:{:04x}",
+                 device.bus_number(),
+                 device.address(),
+                 device_desc.vendor_id(),
+                 device_desc.product_id());
+    }*/
     opts.optflag("r", "read", "read data");
     opts.optflag("e", "export", "export data");
     let matches = match opts.parse(&args[1..]) {
