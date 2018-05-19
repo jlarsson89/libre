69a9a4b00220080ea21854e7bf84a8e6251297e4
Johan Larsson <johan.larsson1989@gmail.com>
2018-05-19 19:32:19 +0100

    moved libusb bits to reader.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 extern crate getopts;
-extern crate libusb;
 use getopts::Options;
 use std::env;
 
@@ -10,15 +9,6 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let _program = args[0].clone();
     let mut opts = Options::new();
-    let context = libusb::Context::new().unwrap();
-    for mut device in context.devices().unwrap().iter() {
-        let device_desc = device.device_descriptor().unwrap();
-        println!("Bus {:03} Device {:03} ID: {:04x}:{:04x}",
-                 device.bus_number(),
-                 device.address(),
-                 device_desc.vendor_id(),
-                 device_desc.product_id());
-    }
     opts.optflag("r", "read", "read data");
     opts.optflag("e", "export", "export data");
     let matches = match opts.parse(&args[1..]) {
