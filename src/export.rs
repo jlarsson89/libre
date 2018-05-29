473065cd5b7f1578700e7fa55c389acc75cb320b
Johan Larsson <johan.larsson1989@gmail.com>
2018-05-29 20:43:15 +0100

    added structs

--- src/export.rs
+++ src/export.rs
@@ -5,6 +5,29 @@ struct Glucose {
     value: f32
 }
 
+struct Basal_injection {
+    time: String,
+    units: f32
+}
+
+struct Bolus_injection {
+    time: String,
+    units: f32
+}
+
+struct Carbs {
+    time: String,
+    grams: i32
+}
+
+struct Exercise {
+    time: String
+}
+
+struct Medication {
+    time: String
+}
+
 pub fn export_data() {
     println!("export");
 }
